8d014bf3e7c07dc2b77fa95e1b8e6a9d606109a5
binbashbanana <101137109+chippyhawk@users.noreply.github.com>
2023-01-06 19:37:49 -0500

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -3,6 +3,13 @@
 // Get the URL to be proxied
 $url = $_GET['url'];
 
+// Validate the URL
+if (!filter_var($url, FILTER_VALIDATE_URL)) {
+    // The URL is not valid, return an error message
+    http_response_code(400);
+    die("Error: Invalid URL");
+}
+
 // Set the user agent to use
 $user_agent = 'MyProxy/1.0';
 
@@ -16,22 +23,30 @@ curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 
+// Set the host header to use your domain
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "Host: chicago2.org"
+));
+
 // Execute the request and get the response
 $response = curl_exec($ch);
 
 // Check for errors
 if (curl_errno($ch)) {
-    // An error occurred, print the error message
-    print curl_error($ch);
+    // An error occurred, return an error message
+    http_response_code(500);
+    die("Error: " . curl_error($ch));
 } else {
     // No errors, check the HTTP status code
     $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     if ($status_code == 200) {
-        // The request was successful, print the response
+        // The request was successful, return the response
+        header("Content-Type: text/html");
         print $response;
     } else {
-        // The request was not successful, print the status code
-        print "Status code: $status_code\n";
+        // The request was not successful, return the status code
+        http_response_code($status_code);
+        die("Error: HTTP status code $status_code");
     }
 }
 
